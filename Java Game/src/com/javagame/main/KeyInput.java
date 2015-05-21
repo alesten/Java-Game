bbce7f825ed7b4cdda266083964f12c5b8c4b6e3
Alexander Steen <ale.steen@hotmail.com>
2015-05-22 01:29:22 +0200

    Added KeyInput class

---
+++ Java Game/src/com/javagame/main/KeyInput.java
@@ -0,0 +1,55 @@
+package com.javagame.main;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+
+    private Handler handler;
+
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        for (GameObject object : handler.objects) {
+            if (object.getId() == ID.Player) {
+                if (key == KeyEvent.VK_W) {
+                    object.setVelY(-5);
+                }
+                if (key == KeyEvent.VK_S) {
+                    object.setVelY(5);
+                }
+                if (key == KeyEvent.VK_D) {
+                    object.setVelX(5);
+                }
+                if (key == KeyEvent.VK_A) {
+                    object.setVelX(-5);
+                }
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        for (GameObject object : handler.objects) {
+            if (object.getId() == ID.Player) {
+                if (key == KeyEvent.VK_W) {
+                    object.setVelY(0);
+                }
+                if (key == KeyEvent.VK_S) {
+                    object.setVelY(0);
+                }
+                if (key == KeyEvent.VK_D) {
+                    object.setVelX(0);
+                }
+                if (key == KeyEvent.VK_A) {
+                    object.setVelX(0);
+                }
+            }
+        }
+    }
+}
